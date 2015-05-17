f70d9eb69a4b4a927242d74a5d4253ae76a14489
hmiha <hajimemihara1108@gmail.com>
2015-05-18 01:50:53 +0900

    add system/

---
+++ system/conf.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+consumer_key        =  '6VflizqzpHBX6Lkxtod3p3ty3'
+consumer_secret     = '9NEwn8Cc2u44NnDLlWml8KIpLQmOHGNDl6GnG5T09M7XKY5EEB'
+access_token_key    = '448831066-mbYVsns6ycvUjH5Q3OvtdXe911yIru3f7lAr1HdV'
+access_token_secret = 'TiDx1aIlsrN13zPX0PekjvJTgujh94QH4czhm54xF0ky6'
